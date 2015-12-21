031e6511db0ad3678ba837076f1794ad15a48981
Martynova <i.martynova@ocsico.local>
2015-12-21 16:16:45 +0300

    21_1_12_15

--- src/js3.js
+++ src/js3.js
@@ -6,7 +6,7 @@
 var data = [
     {id:1, soung:"music/kolobok.mp3", year: "1983", singer:"детские песни"},
     {id:2, soung:"music/otulibki.mp3", year:"1955", singer:"детские песни"},
-    {id:3, soung:"music/kukla.mp3", year:"1984", singer:"детс песня"}
+    {id:3, soung:"music/kukla.mp3", year:"1981", singer:"детс песня"}
 ];
 var AudioBox = React.createClass({
     propTypes: {
@@ -68,7 +68,10 @@ var ListBox = React.createClass({
         return {
             soung: 'soung',
             year: 'year',
-            singer: 'singer'}
+            singer: 'singer',
+            data: this.props.data,
+            }
+
 
     },
     handleClick: function(i){
@@ -77,71 +80,47 @@ var ListBox = React.createClass({
         console.log(this.props.onClickPlay(i));
 
     },
-    componentDidMount:function(e){
 
-    },
-    sortData:function(e){
-      //var sortData = this.props.data;
-      //  console.log(sortData);
-        // sortData.sort()
-        //// {
-        ////    return td1.soung-td2.soung;
-        ////});
-        //for(var i=0; i < sortData.length; i++){
-        //    console.log(sortData[i].soung);
-        //}
-       //this.refs.tableEl;
-        var table=document.querySelector(".table-striped");
-        console.log(e.target.getAttribute('data-type'));
-
-        console.log(e.target.cellIndex);
-        var colNum=e.target.cellIndex;
-        var type =  e.target.getAttribute('date-type');
-
-        sortTable(colNum,type);
-        console.log(table);
-        table.onclick = function(e){
-            if(e.target.tagName != "TH")return;
-        };
+    sortSoung:function(){
 
 
-
-            function sortTable(colNum, type){
-                var tbody = table.getElementsByTagName('tbody')[0];
-                //console.log(tbody);
-                var rowArray = [].slice.call(tbody.rows);
-                console.log(rowArray);
-                var compare;
-                switch (type){
-                    case 'number':
-                        compare = function(rowA, rowB){
-                            return rowA.cells[colNum].innerHTML - rowB.cells[colNum].innerHTML;
-                        };
-                        break;
-                    case 'string':
-                        compare = function(rowA, rowB){
-                            return rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML ? 1: -1;
-                        };
-                        break;
-                }
-                console.log(rowArray.sort(compare));
-                table.removeChild(tbody);
-                for (var i=0; i<rowArray.length; i++){
-                    tbody.appendChild(rowArray[i]);
-                }
-                table.appendChild(tbody);
+            function sortTableSoung(a, b){
+                return a.soung > b.soung ? 1: -1;
             }
 
+        var sortArray = this.state.data.sort(sortTableSoung);
+        this.setState({data: sortArray});
+
+    },
+    sortYear: function(){
+      var array = this.state.data;
+          function sortTableYear(a,b) {
+              return a.year-b.year;
+            }
+      var sortArray=array.sort(sortTableYear);
+      this.setState({data: sortArray});
+    },
+    sortSinger: function(){
+
+            function sortTableSoung(a, b){
+                return a.singer > b.singer ? 1: -1;
+            }
+
+        var sortArray = this.state.data.sort(sortTableSoung);
+        this.setState({data: sortArray});
     },
 
     render: function(){
+        //var stat = this.state.data;
+        //console.log(stat);
+
 
         var nameS = this.props.data.map(function(nameSoung){
-            console.log(nameS);
-            console.log(nameSoung);
+           // console.log(nameS);
+           // console.log(nameSoung);
             return(
                 <tr className="listBox">
-                    <td key={nameSoung.id}>
+                    <td>
                         <a  href="#" data-src={nameSoung.soung} onClick={this.handleClick.bind(this,nameSoung)} >
                             {nameSoung.soung}
                         </a>
@@ -159,14 +138,14 @@ var ListBox = React.createClass({
             <table className="table-striped" >
                 <thead>
                 <tr >
-                    <th data-type="string"  onClick = {this.sortData}>{this.state.soung}</th>
-                    <th data-type="number">{this.state.year}</th>
-                    <th>{this.state.singer}</th>
+                    <th onClick = {this.sortSoung}>{this.state.soung}</th>
+                    <th onClick = {this.sortYear}>{this.state.year}</th>
+                    <th onClick = {this.sortSinger}>{this.state.singer} </th>
                 </tr>
                 </thead>
-
+                <tbody>
                 {nameS}
-
+                </tbody>
             </table>
         );
     }
