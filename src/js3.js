46d05a77d8d2b8c5ebf304de70ecf1453d2d8533
Martynova <i.martynova@ocsico.local>
2016-01-05 10:23:04 +0300

    05_1_01_16

--- src/js3.js
+++ src/js3.js
@@ -1,42 +1,42 @@
-
 /**
  * Created by i.martynova on 12/17/2015.
  */
 //var Alert = require('react-bootstrap/lib/Alert');
 var data = [
-    {id:"1", soung:"music/kolobok.mp3", year: "1983", singer:"детские песни"},
-    {id:"2", soung:"music/otulibki.mp3", year:"1955", singer:"детские песни"},
-    {id:"3", soung:"music/kukla.mp3", year:"1981", singer:"детс песня"},
-    {id:"4", soung:"music/ja_svoboden.mp3", year:"1997", singer:"кипелов"},
-    {id:"5", soung:"music/shtil.mp3", year:"2001", singer:"кипелов"},
-    {id:"6", soung:"music/svoboda.mp3", year:"2002", singer:"кипелов"},
-    {id:"7", soung:"music/poterjannij_raj.mp3", year:"1999", singer:"кипелов"},
-    {id:"8", soung:"music/angelskaja_pil.mp3", year:"1995", singer:"ночь короче дня, кипелов"},
-    {id:"9", soung:"music/begi_za_solncem.mp3", year:"1998", singer:"генератор зла, кипелов"},
-    {id:"10", soung:"music/prorok.mp3", year:"2001", singer:"кипелов"},
-    {id:"11", soung:"music/ne_ver_mne.mp3", year:"1991", singer:"кровь за кровь, кипелов"}
+    {id: "1", soung: "music/kolobok.mp3", year: "1983", singer: "детские песни"},
+    {id: "2", soung: "music/otulibki.mp3", year: "1955", singer: "детские песни"},
+    {id: "3", soung: "music/kukla.mp3", year: "1981", singer: "детс песня"},
+    {id: "4", soung: "music/ja_svoboden.mp3", year: "1997", singer: "кипелов"},
+    {id: "5", soung: "music/shtil.mp3", year: "2001", singer: "кипелов"},
+    {id: "6", soung: "music/svoboda.mp3", year: "2002", singer: "кипелов"},
+    {id: "7", soung: "music/poterjannij_raj.mp3", year: "1999", singer: "кипелов"},
+    {id: "8", soung: "music/angelskaja_pil.mp3", year: "1995", singer: "ночь короче дня, кипелов"},
+    {id: "9", soung: "music/begi_za_solncem.mp3", year: "1998", singer: "генератор зла, кипелов"},
+    {id: "10", soung: "music/prorok.mp3", year: "2001", singer: "кипелов"},
+    {id: "11", soung: "music/ne_ver_mne.mp3", year: "1991", singer: "кровь за кровь, кипелов"}
 
 
 ];
 
 var AudioBox = React.createClass({
 
-    getDefaultProps: function(){
-        return{
+    getDefaultProps: function () {
+        return {
             className: 'player',
         }
     },
-    getInitialState: function(){
+    getInitialState: function () {
         return {
             src: '',
-            pageSize: 10};
+            pageSize: 10
+        };
     },
     //componentWillMount: function(){
     //    this.setState({src: i.soung})
     //},
 
-    handlePlay: function(i){
-        console.log('My play '+i.soung);
+    handlePlay: function (i) {
+        console.log('My play ' + i.soung);
 
         this.setState({src: i.soung});
         console.log(this.refs.audioEl);
@@ -44,32 +44,29 @@ var AudioBox = React.createClass({
         this.refs.audioEl.load();
 
 
-
     },
-    componentDidMount: function(prevProps, prevState){ //вызывается сразу после монтирования. Инициализация этого требует, чтобы DOM узлы шли сюда
+    componentDidMount: function (prevProps, prevState) { //вызывается сразу после монтирования. Инициализация этого требует, чтобы DOM узлы шли сюда
         this.refs.audioEl.load();
     },
-    render: function() {
+    render: function () {
         var music = this.state.src;
         var arrayMusic = music.split();
 
 
-
         return (
             <div className="audioBox">
-                {arrayMusic.map(function(item){
-                    return(
+                {arrayMusic.map(function (item) {
+                    return (
 
                         <audio controls ref="audioEl">
-                            <source src={item}  type="audio/mp3"/>
+                            <source src={item} type="audio/mp3"/>
                         </audio>
 
                     )
                 })}
 
 
-
-                <ListBox data={this.props.data}  onClickPlay={this.handlePlay}/>
+                <ListBox data={this.props.data} handlePlay={this.handlePlay}/>
 
             </div>
         );
@@ -78,7 +75,7 @@ var AudioBox = React.createClass({
 
 
 var ListBox = React.createClass({
-    getInitialState: function(){
+    getInitialState: function () {
         return {
             soung: 'soung',
             year: 'year',
@@ -86,28 +83,28 @@ var ListBox = React.createClass({
             number: '№',
             data: this.props.data,
             className: 'glyphicon glyphicon glyphicon-sort',
-            pageSize:10,
+            pageSize: 10,
             currentPage: 1,
 
-            }
+        }
 
 
     },
-    handleClick1: function(i){
+    handleClick1: function (i) {
         console.log(i);
-        console.log('You clicked: '+ i.soung);
-        console.log(this.props.onClickPlay(i));
+        console.log('You clicked: ' + i.soung);
+        console.log(this.props.handlePlay(i));
 
     },
 
-    sortSoung:function(e){
+    sortSoung: function (e) {
 
-            function sortTableSoung(a, b){
-                return a.soung > b.soung ? 1: -1;
-            }
+        function sortTableSoung(a, b) {
+            return a.soung > b.soung ? 1 : -1;
+        }
 
         var sortArray = this.state.data.sort(sortTableSoung);
-        if(e.target.tagName == 'SPAN'){
+        if (e.target.tagName == 'SPAN') {
             this.setState({className: 'glyphicon glyphicon-sort-by-alphabet'});
         }
 
@@ -115,29 +112,32 @@ var ListBox = React.createClass({
         this.setState({data: sortArray});
 
     },
-    sortYear: function(){
-      var array = this.state.data;
-          function sortTableYear(a,b) {
-              return a.year-b.year;
-            }
-      var sortArray=array.sort(sortTableYear);
-      this.setState({data: sortArray});
+    sortYear: function () {
+        var array = this.state.data;
+
+        function sortTableYear(a, b) {
+            return a.year - b.year;
+        }
+
+        var sortArray = array.sort(sortTableYear);
+        this.setState({data: sortArray});
     },
-    sortId: function(){
+    sortId: function () {
         var array = this.state.data;
 
 
-        function sortTableId(a,b){
-            return a.id-b.id;
+        function sortTableId(a, b) {
+            return a.id - b.id;
         }
-        var sortArray=array.sort(sortTableId);
+
+        var sortArray = array.sort(sortTableId);
         this.setState({data: sortArray});
     },
-    sortSinger: function(){
+    sortSinger: function () {
 
-            function sortTableSoung(a, b){
-                return a.singer > b.singer ? 1: -1;
-            }
+        function sortTableSoung(a, b) {
+            return a.singer > b.singer ? 1 : -1;
+        }
 
         var sortArray = this.state.data.sort(sortTableSoung);
         this.setState({
@@ -146,44 +146,47 @@ var ListBox = React.createClass({
         });
         console.log(sortArray);
     },
-    getPage:function() {
+    getPage: function () {
         var start = this.state.pageSize * (this.state.currentPage - 1);
         var end = this.state.pageSize;
 
 
-        return{
-           newDat: this.state.data.slice(start, end),
-           numPages: this.getNumPages(),
+        return {
+            newDat: this.state.data.slice(start, end),
+            numPages: this.getNumPages(),
 
         }
 
     },
-    getNumPages:function(){
+    getNumPages: function () {
 //console.log(this.state.pageSize);
 
-        var numPages=Math.floor(this.props.data.length / this.state.pageSize);
+        var numPages = Math.floor(this.props.data.length / this.state.pageSize);
 
 
-        if(this.props.data.length % this.state.pageSize > 0){
+        if (this.props.data.length % this.state.pageSize > 0) {
             numPages++
         }
-       // console.log(numPages);
+        // console.log(numPages);
         return numPages;
     },
-    render: function(){
+    getClickPage: function (page) {
+            console.log("My page in getClickPage: "+ page.numPage);
+    },
+    render: function () {
 
-var page = this.getPage();
-       console.log(page.newDat);
+        var page = this.getPage();
+        console.log(page.newDat);
 
-        var nameS =page.newDat.map(function(nameSoung){
+        var nameS = page.newDat.map(function (nameSoung) {
 
-            return(
+            return (
                 <tr className="listBox">
                     <td>
                         {nameSoung.id}
                     </td>
                     <td>
-                        <a  href="#" data-src={nameSoung.soung} onClick={this.handleClick1.bind(this,nameSoung)} >
+                        <a href="#" data-src={nameSoung.soung} onClick={this.handleClick1.bind(this,nameSoung)}>
                             {nameSoung.soung}
                         </a>
                     </td>
@@ -195,23 +198,27 @@ var page = this.getPage();
                     </td>
                 </tr>
             );
-        },this);
+        }, this);
         return (
             <div>
-            <table className="table-striped" >
-                <thead>
-                <tr >
-                    <th onClick = {this.sortId}><span className={this.state.className} aria-hidden="true">{this.state.number}</span></th>
-                    <th onClick = {this.sortSoung}><span className={this.state.className} aria-hidden="true">{this.state.soung}</span></th>
-                    <th onClick = {this.sortYear}><span className={this.state.className} aria-hidden="true">{this.state.year}</span></th>
-                    <th onClick = {this.sortSinger}><span className={this.state.className} aria-hidden="true">{this.state.singer}</span> </th>
-                </tr>
-                </thead>
-                <tbody>
-                {nameS}
-                </tbody>
-            </table>
-            <PaginationBox pageSize={this.state.pageSize}  data={this.props.data} />
+                <table className="table-striped">
+                    <thead>
+                    <tr >
+                        <th onClick={this.sortId}><span className={this.state.className}
+                                                        aria-hidden="true">{this.state.number}</span></th>
+                        <th onClick={this.sortSoung}><span className={this.state.className}
+                                                           aria-hidden="true">{this.state.soung}</span></th>
+                        <th onClick={this.sortYear}><span className={this.state.className}
+                                                          aria-hidden="true">{this.state.year}</span></th>
+                        <th onClick={this.sortSinger}><span className={this.state.className}
+                                                            aria-hidden="true">{this.state.singer}</span></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {nameS}
+                    </tbody>
+                </table>
+                <PaginationBox pageSize={this.state.pageSize} data={this.props.data} getClickPage={this.getClickPage}/>
             </div>
         );
     }
@@ -219,48 +226,49 @@ var page = this.getPage();
 
 
 var PaginationBox = React.createClass({
-    getInitialState:function(){
-     return{
+    getInitialState: function () {
+        return {
 
-         data: this.props.data,
-         pages: [
-            {numPage:"<<"},
-            {numPage:"1"},
-            {numPage:"2"},
-            {numPage:">>"}
-         ]
+            data: this.props.data,
+            pages: [
+                {numPage: "<<"},
+                {numPage: "1"},
+                {numPage: "2"},
+                {numPage: ">>"}
+            ]
 
-     }
+        }
 
     },
-    handleClick2:function(i){
-       console.log(i);
+    handleClick2: function (page) {
+        console.log("My page handleClick2: " + page.numPage);
+        this.props.getClickPage(page);// передаю мой page в ListBox;
     },
-    render: function(){
+    render: function () {
 //console.log();
-    var pagePag= this.state.pages.map(function(page){
+        var pagePag = this.state.pages.map(function (page) {
 
-           return(
-               <li>
-                   <a href="#" >
-                       {page.numPage}
-                   </a>
-               </li>
-           )
-        });
-            return(
+            return (
+                <li>
+                    <a href="#" onClick={this.handleClick2.bind(this, page)}>
+                        {page.numPage}
+                    </a>
+                </li>
+            )
+        }, this);
+        return (
 
-                <nav>
-                    <ul className="pagination pagination-sm">
-                        {pagePag}
-                    </ul>
-                </nav>
-                )
+            <nav>
+                <ul className="pagination pagination-sm">
+                    {pagePag}
+                </ul>
+            </nav>
+        )
     }
 })
 
 ReactDOM.render(
-    <AudioBox data={data} />,
+    <AudioBox data={data}/>,
     document.getElementById('audio_div')
 );
 //<li><a onClick={this.handleClick2}>1</a></li>
